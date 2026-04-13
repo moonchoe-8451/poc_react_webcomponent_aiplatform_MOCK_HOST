f0df553740f7a1910f509855cc50e9d2bfd6fe4f
Moon Choe <Moon.Choe@8451.com>
2026-04-13 09:57:36 -0500

    finished commit- embedded r2wc web component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,43 @@
-import { Component, signal } from '@angular/core';
+import {
+  Component,
+  signal,
+  CUSTOM_ELEMENTS_SCHEMA,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import mockMessage from './mockMessage.json';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class App {
-  protected readonly title = signal('react-component-tester');
+export class App implements AfterViewInit {
+  isLoggedIn = true;
+
+  models = ['Claude-4.6', 'GPT-5.4', 'Gemini-2.5'];
+  // apiEndpoint = {
+  //   id: '1',
+  //   role: 'ai',
+  //   content: [{ type: 'text', text: 'Mock response from Angular' }],
+  // };
+  apiEndpoint = mockMessage;
+
+  @ViewChild('chatbox', { static: false }) chatboxRef!: ElementRef;
+
+  ngAfterViewInit() {
+    if (this.chatboxRef && this.chatboxRef.nativeElement) {
+      // Listen for custom event
+      this.chatboxRef.nativeElement.addEventListener('ai-chat-message-sent', (event: any) => {
+        // do anything you want with the event data
+        console.log(
+          'Testing if event is fired upon message sent from chatbox component',
+          event.detail,
+        );
+      });
+    }
+  }
 }
